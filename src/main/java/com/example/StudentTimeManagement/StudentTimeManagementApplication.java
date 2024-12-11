2a63df2f699a1ee77c693f762cebb4b689b43c21
Raresssm <lungurares4@gmail.com>
2024-12-11 18:18:34 +0200

    init commit

---
+++ src/main/java/com/example/StudentTimeManagement/StudentTimeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentTimeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentTimeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentTimeManagementApplication.class, args);
+	}
+
+}
